5e0bb9a754aaaaf26849b7bddcd11a6cc988259a
MorisFromRTU <arme-kara@mail.ru>
2024-09-23 17:31:46 +0300

    fix: deleted test model hello

--- app/database/models.py
+++ app/database/models.py
@@ -2,15 +2,6 @@ from sqlalchemy import Column, Integer, String, DateTime
 from datetime import timezone, datetime
 from app.db import Base
 
-class HelloItem(Base):
-    __tablename__ = "hellos"
-
-    id = Column(Integer, primary_key=True, index=True)
-    title = Column(String, index=True)
-    
-    def __repr__(self) -> str:
-        return f"HelloItem = {self.title!r})"
-
 class User(Base):
     __tablename__ = "users"
 
